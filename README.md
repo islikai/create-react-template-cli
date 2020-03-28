97932948b167db8569b598fa6c2192435fc41413
likai <islikai@163.com>
2020-03-28 12:11:59 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # create-react-template-cli
+
+
+### 使用方法
+npm 安装create-react-template-cli包
+```
+npm install create-react-template-cli
+```
+
+创建模版
+```
+rtc init <fileName>
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node   //,告诉操作系统用node来解释这个脚本
+
+const fs = require('fs');
+
+// 终端输入处理包
+const program = require('commander');
+// 获取package文件
+const package = require('./package.json');
+
+program
+  .version(package.version)
+  .option('-v, --version', '查看版本')
+  .option('init', '创建模版')
+  .command('init <name>')
+  .action(name => {
+    console.log(name);
+  })
+
+program.parse(process.argv);
